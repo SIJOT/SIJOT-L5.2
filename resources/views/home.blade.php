b818d9dcbd76a6a918739863c198c70877f8073d
Tjoosten <Tjoosten3@gmail.com>
2016-03-17 19:32:17 +0100

    Add home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,12 +7,9 @@
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-        <h1>
-            Verhuur
-        </h1>
+        <h1>Info</h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
-            <li><a href="#">Verhuur</a></li>
             <li class="active">Overzicht</li>
         </ol>
     </section>
@@ -20,14 +17,89 @@
     <!-- Main content -->
     <section class="content">
 
-        <!-- Default box -->
-        <div class="box">
-            <div class="box-body">
+        <div class="row">
+            <div class="col-lg-4 col-xs-8">
+                <!-- small box -->
+                <div class="small-box bg-aqua">
+                    <div class="inner">
+                        <h3>40</h3>
 
+                        <p>Verhuringen</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-home"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        <i class="fa fa-arrow-circle-right"></i> info
+                    </a>
+                </div>
             </div>
-            <!-- /.box-body -->
+
+            <div class="col-lg-4 col-xs-8">
+                <!-- small box -->
+                <div class="small-box bg-red">
+                    <div class="inner">
+                        <h3>Wijzig</h3>
+                        <p>Profiel</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-cogs"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        <i class="fa fa-arrow-circle-right"></i> info
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-xs-8">
+                <!-- small box -->
+                <div class="small-box bg-green">
+                    <div class="inner">
+                        <h3>7</h3>
+                        <p>Gebruikers</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-user"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        <i class="fa fa-arrow-circle-right"></i> info
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-xs-8">
+                <!-- small box -->
+                <div class="small-box bg-orange">
+                    <div class="inner">
+                        <h3>7</h3>
+                        <p>Items</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-cloud"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        <i class="fa fa-arrow-circle-right"></i> info
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-xs-8">
+                <!-- small box -->
+                <div class="small-box bg-yellow">
+                    <div class="inner">
+                        <h3>Uitloggen</h3>
+                        <p>Bye :(</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-sign-out"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        <i class="fa fa-arrow-circle-right"></i> info
+                    </a>
+                </div>
+            </div>
+
         </div>
-        <!-- /.box -->
 
     </section>
     <!-- /.content -->
