252b55daf8d030648856575f4f7cbe4489b6690b
Tjoosten <Tjoosten3@gmail.com>
2016-03-19 12:27:27 +0100

    fix factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,6 +16,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'name' => $faker->name,
         'email' => $faker->email,
         'gsm' => $faker->phoneNumber,
+        'images' => $faker->word,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
