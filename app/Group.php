d13ec259c1046575401d59bbce869b4d3be59a32
Tjoosten <Tjoosten3@gmail.com>
2016-03-21 18:56:35 +0100

    Fix #75

--- app/Group.php
+++ app/Group.php
@@ -7,6 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Group extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['Uri', 'title', 'sub_title', 'description'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['created_at', 'updated_at'];
+
+    /**
      * @param $query
      * @param string $uri the uri segment.
      * @return mixed
