55aa3ff70c88a3206e8521392312c4376c6a7245
Tjoosten <Tjoosten3@gmail.com>
2016-03-24 18:47:18 +0100

    add bootstrap js to the index page.
    	modified:   resources/views/front-end/home.blade.php

--- resources/views/front-end/home.blade.php
+++ resources/views/front-end/home.blade.php
@@ -129,5 +129,9 @@
     </footer>
 </div>
 
+{{-- Javascript in the bottom of the file. --}}
+{{-- So the page loads faster --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 </body></html>
