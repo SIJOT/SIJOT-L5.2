ed594a7656b1f470a2f2c7420af811eae7c14aaf
Tjoosten <Tjoosten3@gmail.com>
2016-03-29 01:47:41 +0200

    Fix wrong heading

--- resources/views/backend/users/insert.blade.php
+++ resources/views/backend/users/insert.blade.php
@@ -8,7 +8,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            Verhuur
+            Nieuwe gebruiker
         </h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
