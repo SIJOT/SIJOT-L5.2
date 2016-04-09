e50552330bd47de1e9d0c139a946d887807c58c3
vagrant <vagrant@sijot-l5-24>
2016-04-09 23:48:22 +0000

    Add rental front-end tests

--- tests/RentalBackEndTest.php
+++ tests/RentalBackEndTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class RentalTest extends TestCase
+class RentalBackEndTest extends TestCase
 {
     /**
      * A basic test example.
