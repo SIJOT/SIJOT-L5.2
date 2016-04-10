555d72bbedca79f4251521be835d5656981c5ac6
Tjoosten <Tjoosten3@gmail.com>
2016-04-10 02:39:36 +0200

    Add rental backend tests

--- tests/RentalBackEndTest.php
+++ tests/RentalBackEndTest.php
@@ -1,18 +1,133 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Artisan;
 
 class RentalBackEndTest extends TestCase
 {
+    use DatabaseMigrations, DatabaseTransactions;
+
     /**
-     * A basic test example.
+     * GET: backend/rental/overview/all
+     * GET: backend/rental/overview/new
+     * GET: backend/rental/overview/option
      *
-     * @return void
+     * @group all
+     * @group rental
      */
-    public function testExample()
+    public function testOverview()
     {
-        $this->assertTrue(true);
+        $user = factory(App\User::class)->create();
+
+        Artisan::call('bouncer:seed');
+        $role = Bouncer::assign('verhuur')->to($user);
+        $this->assertTrue($role);
+
+        $new = $this->actingAs($user);
+        $new->visit('backend/rental/overview/all');
+        $new->seeStatusCode(200);
+
+        $all = $this->actingAs($user);
+        $all->visit('backend/rental/overview/new');
+        $all->seeStatusCode(200);
+
+        $option = $this->actingAs($user);
+        $option->visit('backend/rental/overview/new');
+        $option->seeStatusCode(200);
+    }
+
+    /**
+     * GET: backend/rental/destroy/{id}
+     *
+     * @group all
+     * @group rental
+     */
+    public function testDestroy()
+    {
+        $user = factory(App\User::class)->create();
+        $rent = factory(App\Rental::class)->create();
+
+        Artisan::call('bouncer:seed');
+        $role = Bouncer::assign('verhuur')->to($user);
+        $this->assertTrue($role);
+
+        $this->actingAs($user)
+            ->visit('backend/rental/destroy/' . $rent->id)
+            ->seeStatusCode(200)
+            ->notSeeInDatabase('rentals', ['deleted_at' => null, 'id' => $rent->id]);
+    }
+
+    /**
+     * GET: backend/rental/option/{id}
+     *
+     * @group all
+     * @group rental
+     */
+    public function testOption()
+    {
+        $rent = factory(App\Rental::class)->create(['Status' => 0]);
+        $user = factory(App\User::class)->create();
+
+        Artisan::call('bouncer:seed');
+        $role = Bouncer::assign('verhuur')->to($user);
+        $this->assertTrue($role);
+
+        $notDb['Status'] = $rent->Status;
+        $notDb['id']     = $rent->id;
+
+        $Db['Status']    = 1;
+        $Db['id']        = $rent->id;
+
+        $this->actingAs($user)
+            ->visit('backend/rental/option/'. $rent->id)
+            ->seeStatusCode(200)
+            ->seeInDatabase('rentals', $Db)
+            ->notSeeInDatabase('rentals', $notDb);
+    }
+
+    /**
+     * GET: backend/rental/confirm/{id}
+     *
+     * @group all
+     * @group rental
+     */
+    public function testConfirm()
+    {
+        $rent = factory(App\Rental::class)->create(['Status' => 0]);
+        $user = factory(App\User::class)->create();
+
+        Artisan::call('bouncer:seed');
+        $role = Bouncer::assign('verhuur')->to($user);
+        $this->assertTrue($role);
+
+        $notDb['Status'] = $rent->Status;
+        $notDb['id']     = $rent->id;
+
+        $Db['Status']    = 2;
+        $Db['id']        = $rent->id;
+
+        $this->actingAs($user)
+            ->visit('backend/rental/confirm/'. $rent->id)
+            ->seeStatusCode(200)
+            ->seeInDatabase('rentals', $Db)
+            ->notSeeInDatabase('rentals', $notDb);
+    }
+
+    /**
+     * GET: backend/rental/download
+     *
+     * @group all
+     * @group rental
+     */
+    public function testDownload()
+    {
+        $user = factory(App\User::class)->create();
+
+        Artisan::call('bouncer:seed');
+        $role = Bouncer::assign('verhuur')->to($user);
+        $this->assertTrue($role);
+
+        $this->actingAs($user)->visit('backend/rental/download');
     }
 }
