6c91598c37e6b5ae4c98b6c01f14bb0109033876
Tjoosten <Tjoosten3@gmail.com>
2016-04-11 11:20:38 +0200

    Add properties to the user models

--- app/User.php
+++ app/User.php
@@ -6,6 +6,13 @@ use Fenos\Notifynder\Notifable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Silber\Bouncer\Database\HasRolesAndAbilities;
 
+/**
+ * @property mixed id,       The record id in the database. (auto-increment)
+ * @property mixed name,     The name from the user.
+ * @property mixed gsm,      The gsm number from the user.
+ * @property mixed email,    The email address from the user.
+ * @property mixed password, The password from the user.
+ */
 class User extends Authenticatable
 {
     use HasRolesAndAbilities, Notifable;
