32cb45bf8ee898e19aea48b045c050040ab2796a
Tjoosten <Tjoosten3@gmail.com>
2016-04-14 23:21:40 +0200

    Add blocked email template

---
+++ resources/views/emails/blocked.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Notificatie: Account geblokkeerd.</title>
+</head>
+<body>
+	<p>
+		Vie deze weg brengen wij u op de hoogte. Dat uw account op 
+		<a href="http://www.st-joris-turnhout.be">http://www.st-joris-turnhout.be</a>
+		is geblokkeerd. Vind u dit onterecht kunt u dit bespreken met de Groepsploeg/Hoofdleiidng.
+	</p>
+
+	<hr>
+	<p class="small text-muted">
+		Deze mail notificatie is afkomstig van <a href="http://www.st-joris-turnhout.be">www.st-joris-turnhout.be</a> <br>
+	</p>
+</body>
+</html>
